8364ecc700fde2b86246c6cb77a29c469ea8c986
bek <somonkulov2018@gmail.com>
2018-11-28 14:33:37 -0500

    fixing bugs

--- javascript/loadList.js
+++ javascript/loadList.js
@@ -1,19 +1,17 @@
 console.log("loadList.js started!");
-let path = "../Artists/ozodbek.json";
+let path = "./Artists/ozodbek.json";
 
 
 $.getJSON(path, function(json){
     if(json != null){
-        console.log(json);
-        
+        console.log(json.length);
+
+        for(let i=0; i < json.length; i++){
+            let newDiv = document.createElement("div");
+            newDiv.appendChild(document.createTextNode("Test"));
+            document.getElementById("songsList").appendChild(newDiv);
+            console.log("test");
+        }
     }
 });
 
-
-
-for(let i=0; i < 20; i++){
-    let newDiv = document.createElement("div");
-    //newDiv.appendChild(document.createTextNode("Test"));
-    document.getElementById("songsList").appendChild(newDiv);
-    console.log("test");
-}
